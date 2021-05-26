1df208dd419ac2dc4d28a3529fe4e5de5a397fe6
joao-rafael <joaorafa131@gmail.com>
2021-05-26 18:21:10 -0300

    feat(naive): naive multiplication now working

--- naive.py
+++ naive.py
@@ -1,13 +1,24 @@
+# cria matriz vazia 
+def createResult(n, m):
+  result = []
 
+  for c in range(n):
+    result.append([])
+
+    for d in range(m):
+      result[c].append(0)
+
+  return result
+
+#multiplica matriz
 def multiply(a, b):
-  print(a)
-  result = [[]]
+  result = createResult(len(a), len(b[0]))
 
   #itera sob as linhas de a
   for i in range(len(a)):
 
-    #colunas de b
-    for j in range(len(b)):
+    #colunas de b, apenas para o primeiro elemento
+    for j in range(len(b[0])):
 
       for k in range(len(b)):
         
